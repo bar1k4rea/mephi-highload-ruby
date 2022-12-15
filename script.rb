ae33960e889f72db56376b46c695fa64e43be1fb
Barabanov Andrey <bbarabanow@ya.ru>
2022-12-16 01:33:12 +0300

    add find_prime_numbers, merge_sort and mult_matrix

--- script.rb
+++ script.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'matrix'
 
 class Time
   def to_ms
@@ -39,12 +40,14 @@ def lcg(flag)
 end
 
 
+
+
 # Finding the maximum in the array
 def find_max(flag)
-  print("Input seed: ")
-  seed = gets.to_i
   print("Input array size: ")
   size = gets.to_i
+  print("Input seed: ")
+  seed = gets.to_i
 
   max = 0
   i = 0
@@ -69,27 +72,192 @@ def find_max(flag)
 end
 
 
+
+
 # Sorting by merging an array
+def merge_sort(unsorted_array)
+  if unsorted_array.length <=1
+    unsorted_array
+  else
+    mid = unsorted_array.length/2
+    first_half = merge_sort(unsorted_array.slice(0...mid))
+    second_half = merge_sort(unsorted_array.slice(mid...unsorted_array.length))
+    merge(first_half, second_half)
+  end
+end
+def merge(left_array, right_array)
+  sorted_array = []
+  while !left_array.empty? && !right_array.empty? do
+    if left_array[0] < right_array[0]
+      sorted_array.push(left_array.shift)
+    else
+      sorted_array.push(right_array.shift)
+    end
+  end
+  sorted_array.concat(left_array).concat(right_array)
+end
 def merge_sort_array(flag)
+  print("Input array size: ")
+  size = gets.to_i
+  print("Input seed: ")
+  seed = gets.to_i
+
+  srand seed
+  my_array = Array.new(size) { rand 1_073_741_824 }
+
+  start_time = Time.now
+
+  my_array = merge_sort(my_array)
+
+  end_time = Time.now
+
+  print "time: " + (end_time.to_ms - start_time.to_ms).to_s + "ms\n"
+
+  if flag == "1"
+    puts my_array
+  end
 
 end
 
 
+
+
 # Sorting by merging a linked list
+class Node
+  attr_accessor :data, :next
+  def initialize(value)
+    @data = value
+    @next = nil
+  end
+end
+def mergesort(head)
+  return head if !head || !head.next
+
+  a, b = front_back_split(head)
+  a = mergesort(a)
+  b = mergesort(b)
+  sort_merge(a, b)
+end
+def front_back_split(head)
+  slow = head
+  fast = head.next
+  until fast.nil?
+    fast = fast.next
+    unless fast.nil?
+      slow = slow.next
+      fast = fast.next
+    end
+  end
+  a = head
+  b = slow.next
+  slow.next = nil
+  [a, b]
+end
+def sort_merge(a, b)
+  result = nil
+
+  if a.nil?
+    return b
+  elsif b.nil?
+    return a
+  end
+
+  if a.data <= b.data
+    result = a
+    result.next = sort_merge(a.next, b)
+  else
+    result = b
+    result.next = sort_merge(a, b.next)
+  end
+  result
+end
 def merge_sort_linked_list(flag)
 
 end
 
 
+
+
+
 # Multiplication of matrices of size N*N
 def mult_matrix(flag)
+  print("Input array size: ")
+  size = gets.to_i
+  print("Input seed: ")
+  seed = gets.to_i
+
+  srand seed
+  a = Matrix.build(size) { rand 100 }
+  b = Matrix.build(size) { rand 100 }
+
+  start_time = Time.now
+
+  c = a * b
+
+  end_time = Time.now
+
+  print "time: " + (end_time.to_ms - start_time.to_ms).to_s + "ms\n"
+
+  if flag == "1"
+    (0..size).each { |i|
+      (0..size).each { |j|
+        print(c[i, j])
+        print(" ")
+      }
+      print("\n")
+    }
+  end
 
 end
 
 
-# Search for prime numbers
-def find_prime_num(flag)
+def find_prime_numbers(flag)
+  a = gets.to_i
+  b = gets.to_i
 
+  if b < 60184
+    size = 6076
+  elsif a < 5393
+    size = b/(Math.log(b) - 1.1)
+  else
+    size = b/(Math.log(b) - 1.1) - a/(Math.log(a) - 1)
+  end
+
+  counter = 0
+  array = Array.new(size.to_i)
+
+  start_time = Time.now
+
+  (a..b - 1).each do |n|
+    if isPrime(n)
+      array[counter] = n
+      counter += 1
+    end
+  end
+
+  end_time = Time.now
+
+  print "time: " + (end_time.to_ms - start_time.to_ms).to_s + "ms\n"
+
+  if flag == "1"
+    (0..counter - 1).each do |i|
+      print("#{array[i]} ")
+    end
+  end
+
+end
+
+
+def isPrime(n)
+  if n <= 1
+    return false
+  end
+  (2..Math.sqrt(n).to_i).each do |i|
+    if n % i == 0
+      return false
+    end
+  end
+  true
 end
 
 
@@ -98,11 +266,6 @@ def read_and_write_file(flag)
 
 end
 
-
-# Array and generator processing functions
-def timing_arr_func(flag)
-
-end
 
 
 print "Input program id: "
@@ -122,11 +285,9 @@ when "2.1"
 when "3.1"
   mult_matrix(flag)
 when "4.1"
-  find_prime_num(flag)
+  find_prime_numbers(flag)
 when "5.1"
   read_and_write_file(flag)
-when "6.1"
-  timing_arr_func(flag)
 else
   # puts "Error in the program id!"
 end
